d8531280a04b6ae0972e8d2e09d255b624032503
nichol alexander <nichol.alexander@gmail.com>
2016-02-19 12:48:50 -0500

    panel renames, about page, working clock

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+window.onload = function() {
+  
+  // execute every second:
+  setInterval(function(){
+    // display current time
+    var currentTime = moment().format('h:mm:ss a');
+    $('.time').html(currentTime);
+
+    // set current hour
+    var currentHour = moment().hour();
+
+    // set current panel
+    var currentPanel = moment().hour() + '.jpg';
+    var path = 'images/panels/'
+    var panelFile = path + currentPanel;
+    $('#panel-image').attr('src', panelFile);
+
+
+    // if (moment().hour() != currentHour) {
+
+    // };
+  }, 1000);
+
+
+  
+};
